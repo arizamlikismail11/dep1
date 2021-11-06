8b5074b9dafa613186aa4d619d5e5d97d3f3411d
rezza fauzi <rezzaf205@gmail.com>
2021-11-06 15:17:56 +0700

    menambahkan file latihan22.py

---
+++ latihan22.py
@@ -0,0 +1,11 @@
+#contoh cara mengurukan data di dalam list python
+
+#inisialisasi variabel luas dengan nilai berformat list
+luas = [34, 56, 1, 3, 4, 100]
+
+#urutkan data di dalam list dari yang terkecil ke yang terbesar
+luas.sort()
+ 
+#tampilkan nilai dari variabel luas setelah pengurutan
+print(luas)
+[1, 3, 4, 34, 56, 100]
