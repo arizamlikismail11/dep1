593aa184c337b7a1cbcb5ff6f7cbb5a0c0b81ce1
rezza fauzi <rezzaf205@gmail.com>
2021-11-06 15:09:34 +0700

    menambahkan file latihan11.py

---
+++ latihan11.py
@@ -0,0 +1,11 @@
+#Kondisi if adalah kondisi yang akan di eksisusi oleh program jika bernilai benar atau TRUE
+
+nilai = 9
+
+#jika kondisi benar/TRUE maka program akan mengeksekusi printah dibawahnya
+if(nilai > 7):
+    print("sembilan lebih besar dari angka tujuh") # kondisi benar, dieksekusi
+
+#jika kondisi salah/FALSE maka program tidak akan mengeksekusi perintah dibawanya
+if(nilai > 10):
+    print("sembilan lebih besar dari angka sepuluh") # kondisi salah, maka tidak tereksekusi
